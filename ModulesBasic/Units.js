c6ccb52c4e732e79ff140146d8afe0e0041c1a6b
Dinked <liston.new@gmail.com>
2023-11-05 22:05:10 +0100

    Updated names

--- ModulesBasic/Units.js
+++
@@ -1,5 +0,0 @@
-const sayHi = (names) => {
-    console.log(`Hello ${names}`);
-}
-
-module.exports = sayHi
--- ModulesBasic/modules.js
+++
@@ -1,8 +0,0 @@
-const names = require('./names')
-const sayHi = require('./Units')
-const data = require('./alternative')
-
-console.log(data.items, data.singlePerson.name)
-sayHi(names.john)
-sayHi(names.susan)
-sayHi('DogShit')
